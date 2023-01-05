eff579beb2c067ddb334f4579c6768cef735c68f
David Sherret <dsherret@gmail.com>
2023-01-05 09:36:00 -0500

    fix: incorrect package.json main

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -8,7 +8,6 @@ await build({
     name: "dts-minify",
     version: Deno.args[0],
     description: "Minifier for TypeScript declaration files (.d.ts).",
-    main: "dist/dts-minify.js",
     author: "David Sherret",
     license: "MIT",
     repository: "git+https://github.com/dsherret/dts_minify.git",
